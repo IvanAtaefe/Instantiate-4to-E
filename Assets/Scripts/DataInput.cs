7507f37538e123fa9bd8e872a5f8f0050c651de5
IvanAtaefe <88833116+IvanAtaefe@users.noreply.github.com>
2022-05-10 15:31:59 -0300

    New Scene and Data Input

---
+++ Assets/Scripts/DataInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInput : MonoBehaviour
+{
+    public Text txtUsername;
+    
+    public void saludos (){
+        Debug.Log("Hola " + txtUsername.text);
+    }
+}
