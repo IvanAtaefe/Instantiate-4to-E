61822b473867214d9e30e0f51dc84ea5454665d6
IvanAtaefe <88833116+IvanAtaefe@users.noreply.github.com>
2022-05-10 15:38:14 -0300

    double input field

--- Assets/Scripts/DataInput.cs
+++ Assets/Scripts/DataInput.cs
@@ -7,7 +7,19 @@ public class DataInput : MonoBehaviour
 {
     public Text txtUsername;
     
-    public void saludos (){
+    public void saludos(){
         Debug.Log("Hola " + txtUsername.text);
+    }
+
+    public void doble()
+    {
+        if (txtUsername.text != "")
+        {
+            Debug.Log(int.Parse(txtUsername.text) * 2);
+        }
+        else
+        {
+            Debug.Log("El campo no puede estar vacio");
+        }
     }
 }
